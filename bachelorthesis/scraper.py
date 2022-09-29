ecd94823c9c39ba7408e5e635d3a4e8e69be315e
vfcapital <viktor.fen@gmx.de>
2022-09-30 00:46:48 +0200

    scrape NFT data from etherscan

--- bachelorthesis/scraper.py
+++ bachelorthesis/scraper.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
-CHAINS = [
-    "ethereum",
-]
+CHAIN = "ethereum"
+CATEGORY = "art"
+
+def _get_slugs_link():
+    return f"https://opensea.io/rankings?sortBy=total_volume&chain={CHAIN}&category={CATEGORY}"
+
+def _get_opensea_collection_link(slug):
+    return f"https://opensea.io/collection/{slug}"
 
 
-def _get_link(chain):
-    return f"https://opensea.io/rankings?sortBy=total_volume&chain={chain}"
+def _get_etherscan_link(address):
+    return f"https://etherscan.io/txs?a={address}&f=3"
 
 
-def _get_html():
-    chain = CHAINS[0]
-    link = _get_link(chain)
-
+def _get_html(link):
+    chain = CHAIN
     headers = {
         "Referer": link,
         "sec-ch-ua": '"Chromium";v="104",'
@@ -30,14 +33,15 @@ def _get_html():
     return requests.get(link, headers=headers).content
 
 
-def _extract_js_script():
-    raw_html = _get_html()
+def _extract_js_script(link):
+    raw_html = _get_html(link)
     soup = BeautifulSoup(raw_html, "lxml")
     return soup.findAll("script")[0].string
 
 
 def get_slug_names():
-    script = _extract_js_script()
+    link = _get_slugs_link()
+    script = _extract_js_script(link)
 
     location_slug = -1
     location_logo = -1
@@ -57,3 +61,34 @@ def get_slug_names():
         # Display result.
         slug_names.append(script[location_slug + 8 : location_logo])
     return slug_names
+
+
+def get_collection_address(slug):
+    link = _get_opensea_collection_link(slug)
+    script = _extract_js_script(link)
+
+    location_address = -1
+    location_chain = -1
+
+    address = []
+
+    while True:
+        location_address = script.find('etherscan.io', location_address + 1)
+        location_chain = script.find('","chain', location_chain + 1)
+
+        # Break if not found.
+        if location_address == -1:
+            break
+        if location_chain == -1:
+            break
+
+        # Display result.
+        address.append(script[location_address + 26 : location_chain])
+    return address[0].replace("u002F","")
+
+
+def get_etherscan(address):
+    link = _get_etherscan_link(address)
+    raw_html = _get_html(link)
+    soup = BeautifulSoup(raw_html, "lxml")
+    return soup
